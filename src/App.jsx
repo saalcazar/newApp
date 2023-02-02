d62e00a81f38a1e2c49c92af17510d8ab440bc49
saalcazar <alcazar.samuel@gmail.com>
2023-02-02 16:10:04 -0400

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then(resp => resp.json())
+      .then(data => {
+        setCriptos(data.data)
+      })
+      .catch(() => {
+        console.error("La petición fallo")
+      })
+  }, []) 
+  
+  if(!criptos) return <span>Cargando...</span>
 
   return (
-    <h1>Hola mundo...</h1>
+    <>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {criptos.map(({name, priceUsd}) => (
+          <li>Nombre: {name} Precio: {priceUsd}</li>
+        ))}
+      </ol>
+    </>
   )
 }
 
